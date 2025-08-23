bfb8574428fa41bf38e04ee49d867f3d18105fc0
Juan David Perez <juandevperez@gmail.com>
2025-08-23 02:54:00 -0300

    se agrego la libreria de iconos lucide y se termino la funcionalidad de agregar al carrito en la pagina detalle de producto

--- src/features/products/components/ProductDetail.tsx
+++ src/features/products/components/ProductDetail.tsx
@@ -10,6 +10,28 @@ interface Props {
 export default function ProductDetail({ product }: Props) {
   const { addToCart } = useCart();
   const [cantidad, setCantidad] = useState(1);
+  const [showToast, setShowToast] = useState(false);
+
+  const handleAdd = () => {
+    addToCart(
+      {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        image: product.images?.[0]?.url ?? "",
+      },
+      cantidad
+    );
+
+    // resetear cantidad
+    setCantidad(1);
+
+    // mostrar el toast
+    setShowToast(true);
+
+    // ocultarlo a los 2.5s
+    setTimeout(() => setShowToast(false), 2500);
+  };
 
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded shadow">
@@ -23,37 +45,48 @@ export default function ProductDetail({ product }: Props) {
       <p className="text-xl font-bold text-green-600 mb-4">
         {formatPrice(product.price)}
       </p>
+
+      {/* selectro de cantidad */}
       <div className="flex items-center">
-        <div className="flex gap-3 items-center rounded-lg border-borde border">
-          <button 
-          onClick={() => setCantidad(cantidad - 1)}
-          className="btn btn-md btn-ghost text-xl text-slate-500">
+        <div className="flex gap-2 items-center rounded-lg border-borde border justify-between w-32">
+          <button
+            onClick={() => setCantidad(cantidad - 1)}
+            className={`btn btn-md btn-ghost text-xl ${
+              cantidad <= 1 && "btn-disabled"
+            }`}
+            disabled={cantidad <= 1}
+          >
             -
           </button>
           <span>{cantidad}</span>
-          <button 
-          onClick={() => setCantidad(cantidad + 1)}
-          className="btn btn-md btn-ghost text-xl text-slate-500">
+          <button
+            onClick={() => setCantidad(cantidad + 1)}
+            className={`btn btn-md btn-ghost text-xl ${
+              product.stock === cantidad && "btn-disabled"
+            }`}
+            disabled={cantidad === product.stock}
+          >
             +
           </button>
         </div>
       </div>
+
+      {/* boton agregar al carrito */}
       <button
         className="btn btn-primary mt-3"
-        onClick={() =>
-          addToCart(
-            {
-              id: product.id,
-              name: product.name,
-              price: product.price,
-              image: product.images?.[0]?.url ?? "",
-            },
-            cantidad
-          )
-        }
+        onClick={handleAdd}
       >
         <span className="relative -top-0.5">Agregar al carrito</span>
       </button>
+
+      {/* toast */}
+      {showToast && (
+        <div className="toast toast-top toast-end top-20">
+          <div className="alert alert-success alert-soft">
+            <span>Producto agregado al carrito ✅</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
